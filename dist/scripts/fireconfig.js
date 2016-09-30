6abc02fd612566f1e930f67fdf687684ebcdc9a7
Craig Schotch <caschotchii@gmail.com>
2016-09-29 19:16:34 -0700

    Pushing for Mentor Review

---
+++ dist/scripts/fireconfig.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+
+var config = {
+apiKey: "AIzaSyA8hsYcAXOQ6EtusHiXOVQYr4dxmHElANs",
+authDomain: "bloc-chat-35181.firebaseapp.com",
+databaseURL: "https://bloc-chat-35181.firebaseio.com",
+storageBucket: "bloc-chat-35181.appspot.com",
+messagingSenderId: "524245545526"
+};
+firebase.initializeApp(config);
