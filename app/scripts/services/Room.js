cfff4fb8ef301ec1c9cdbc3664bd06af57b308fa
Craig Schotch <caschotchii@gmail.com>
2016-10-12 19:41:53 -0700

    Added list messages functionality

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -3,12 +3,19 @@
         var ref = firebase.database().ref().child("rooms");
         var rooms = $firebaseArray(ref);
 
+        var refMes = firebase.database().ref().child("messages");
+
         return {
             //array of all rooms
             all: rooms,
             // adds new room
             create: function(room) {
                 rooms.$add({name: room});
+            },
+            getMessages: function(roomId){
+                //get all messages based on current room --- ? need to figure out display before I can test
+                var messages = $firebaseArray(refMes.orderByChild("roomId").equalTo(roomId)); // ??
+                return messages;
             }
         };
     }
--- dist/scripts/controllers/RoomCtrl.js
+++ dist/scripts/controllers/RoomCtrl.js
@@ -2,6 +2,19 @@
     function RoomCtrl(Room, $scope) {
         $scope.rooms = Room.all;
 
+// need to find out how to list all messages
+        $scope.messages = []; // from roomCtrl
+        $scope.currentRoom = {};
+
+// function for ng-click to determine which room we are in while clicking room names
+        $scope.activeRoom = function(room){
+            $scope.currentRoom = room;
+
+            //messages from db
+            $scope.messages = Room.getMessages(room.$id);
+        };
+
+
     }
 
     angular
